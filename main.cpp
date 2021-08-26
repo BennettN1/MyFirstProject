68ec52a17705583bfb3a5871e9f3e23a016b86b2
BennettN1 <89536101+BennettN1@users.noreply.github.com>
2021-08-26 11:25:27 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+ Nick Bennett
+ C++ 2021
+ Lab 1 and Hello World
+ Figuring out the basics of writing the C++ code and understanding what is going on inside of each piece of data that is used inside of XCode
+ */
+ 
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    std::cout << "My favorite color is green\n";
+    return 0;
+}
+
